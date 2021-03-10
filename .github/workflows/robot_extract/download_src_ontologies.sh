00015a44a6fa5ce80adee3c28db65d453d20307c
Castro0o <andre@andrecastro>
2021-03-10 12:35:59 +0100

    location of uo.owl

--- .github/workflows/robot_extract/download_src_ontologies.sh
+++ .github/workflows/robot_extract/download_src_ontologies.sh
@@ -2,6 +2,7 @@
 
 while IFS=" " read -r prefix uri 
 do
-    # echo "prefix:" $prefix "uri:" $uri $prefix.owl
-    wget --quiet $uri -c # download the used ontologies olw files
+    echo "prefix:" $prefix "uri:" $uri $prefix.owl
+    echo `pwd`
+    wget --quiet $uri -c --directory-prefix=`pwd` # download the used ontologies olw files
 done < "ontologies.txt"
